cdacf28026065ac75989798b3839d956738646d4
Rob Ede <robjtede@icloud.com>
2017-08-23 02:36:58 +0100

    modernize codebase

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const { resolve } = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const postcssMiddleware = require('postcss-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const debug = require('debug')('hs:app');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', resolve('views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// middleware setup and use
+// uncomment after placing your favicon in /src
+// app.use(favicon(resolve('src', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  require('node-sass-middleware')({
-    src: path.join(__dirname, 'src'),
-    dest: path.join(__dirname, 'dist'),
-    indentedSyntax: true,
-    sourceMapEmbed: true,
-    sourceMapContents: true
-  })
-);
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(resolve('dist')));
+
+// load and set up routes
+const { router: index } = require('./routes/index');
+const { router: users } = require('./routes/users');
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports = { app };
--- server.js
+++ server.js
@@ -1,86 +1,51 @@
-/**
- * Module dependencies.
- */
-
-var app = require('./app');
-var debug = require('debug')('hacksheffield.co:server');
-var http = require('http');
+const { app } = require('./app');
+const debug = require('debug')('hs:server');
+const { createServer } = require('http');
 
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort (val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
+const { PORT = 3000 } = process.env;
+app.set('port', PORT);
 
 /**
  * Event listener for HTTP server "error" event.
  */
-
-function onError (error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+const onError = err => {
+  if (err.syscall !== 'listen') throw err;
 
   // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+  const handleErrors = {
+    EACCES: () => {
+      console.error(`Port ${PORT} requires elevated privileges`);
       process.exit(1);
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    },
+
+    EADDRINUSE: () => {
+      console.error(`Port ${PORT} is already in use`);
       process.exit(1);
-    default:
-      throw error;
-  }
-}
+    },
+
+    default: err => {
+      throw err;
+    }
+  };
+
+  handleErrors[err.code in handleErrors ? err.code : 'default'](err);
+};
 
 /**
  * Event listener for HTTP server "listening" event.
  */
+const onListening = () => {
+  debug(`Listening on port ${PORT}`);
+};
 
-function onListening () {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+/**
+ * Create HTTP server.
+ */
+const server = createServer(app);
+
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
