8289459be91ddf24457742b5318d104499a9a795
Rob Ede <robjtede@icloud.com>
2017-11-23 15:57:00 +0000

    abstract team info

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const team = require('../db/team');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { team });
 });
 
 module.exports = { router };
