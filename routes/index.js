94e51383fbd9f81c37f6ef0b9fbc73cf4668b094
Simon Fish <si@mon.fish>
2018-02-14 11:33:26 +0000

    Resolve change requests from @robjtede and @dannycjones

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const YAML = require('yamljs');
-console.log(process.env);
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 if (!GITHUB_TOKEN) throw new Error('Supply GitHub token');
 
 const graphql = `{
  organization(login:"hacksheffield") {
-    avatarUrl
     databaseId
     repository(name:"people"){
-      name
       object(expression:"master:_data/people.yml"){
         repository{
           object(expression:"master:_config/team-blacklist.json"){
@@ -28,25 +25,16 @@ const graphql = `{
     teams (first: 100){
       edges {
         node{
-          description
-          teamsUrl
-          url
-          slug
-          combinedSlug
           name
           members (first: 100){
             edges {
               node {
                 name
-                bio
                 url
                 login
-                location
-                bioHTML
                 email
                 avatarUrl
                 websiteUrl
-                isCampusExpert
               }
             }
           }
@@ -61,21 +49,18 @@ router.get('/', (req, res, next) => {
   axios.post('https://api.github.com/graphql',
     { query: graphql }, { headers: {'Authorization': `Bearer ${GITHUB_TOKEN}`} })
     .then((graphqlRes) => {
-      console.log(graphqlRes.data);
       const teams = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node);
-      const people = {};
-      YAML.parse(graphqlRes.data.data.organization.repository.object.text).forEach(person => {
-        people[person.github] = person;
-      });
+      const people = YAML.parse(graphqlRes.data.data.organization.repository.object.text)
+        .reduce((people, person) => {
+          return { people, [person.github]: person };
+        }, {});
       const teamBlacklist = JSON.parse(graphqlRes.data.data.organization.repository.object.repository.object.text);
 
-      console.log(people);
       teams.forEach(team => team.members = team.members.edges.map(edge => edge.node));
-      // const people = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node)[0].members.edges.map(edge => edge.node);
       res.render('index', { title: 'Teams', teams: teams, people, teamBlacklist });
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       res.render('error', {});
     });
 });
