d9e7a04f1919660e189e0b9b47faaf76487d2fb4
Rob Ede <robjtede@icloud.com>
2018-03-01 21:35:55 +0000

    remove async/await syntax
    for easier error handling

--- routes/index.js
+++ routes/index.js
@@ -9,35 +9,42 @@ const YAML = require('yamljs');
 const graphql = fs.readFileSync(path.resolve('routes', 'teams.graphql'), 'utf8');
 
 /* GET home page. */
-router.get('/', async (req, res) => {
-  const graphqlRes = await axios.post(
-    'https://api.github.com/graphql',
-    { query: graphql },
-    { headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } }
-  );
+router.get('/', (req, res) => {
+  axios
+    .post(
+      'https://api.github.com/graphql',
+      { query: graphql },
+      { headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } }
+    )
+    .then(graphqlRes => {
+      const org = graphqlRes.data.data.organization;
+      const files = graphqlRes.data.data.organization.files;
 
-  const org = graphqlRes.data.data.organization;
-  const files = graphqlRes.data.data.organization.files;
+      const teams = org.teams.edges.map(edge => edge.node);
+      const teamBlacklist = JSON.parse(files.blacklist.text);
+      const teamOrder = JSON.parse(files.order.text);
 
-  const teams = org.teams.edges.map(edge => edge.node);
-  const teamBlacklist = JSON.parse(files.blacklist.text);
-  const teamOrder = JSON.parse(files.order.text);
+      const people = YAML.parse(files.people.text).map((people, person) => ({
+        [person.github]: person
+      }));
 
-  const people = YAML.parse(files.people.text).map(
-    (people, person) => ({ [person.github]: person })
-  );
+      teams.forEach(team => {
+        team.members = team.members.edges.map(edge => edge.node);
+      });
 
-  teams.forEach(team => {
-    team.members = team.members.edges.map(edge => edge.node);
-  });
-
-  res.render('index', {
-    title: 'Teams',
-    teams: teams,
-    people,
-    teamBlacklist,
-    teamOrder
-  });
+      res.render('index', {
+        title: 'Teams',
+        teams: teams,
+        people,
+        teamBlacklist,
+        teamOrder
+      });
+    }).catch((error) => {
+      console.error(error);
+      res.render('error', {
+        error
+      });
+    });
 });
 
 module.exports = { router };
