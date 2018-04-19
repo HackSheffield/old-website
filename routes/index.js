d7e347806eceb24766f3498159492e7fbed3fc0f
HughZurname <craig.degouveia@gmail.com>
2018-04-19 11:41:52 +0100

    Changed forEach to map to avoid side effects

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
         [person.github]: person
       }), {});
 
-      teams.forEach(team => {
+      teams.map(team => {
         // unwrap node
         team.members = team.members.edges.map(edge => edge.node);
 
