203f1f4af8e6e984b4b0e582de349882e9dff1cf
Matt Burman <matt@mattburman.com>
2018-07-27 16:14:17 +0100

    Required/optional env var handling

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ const dotenv = require('dotenv');
 const debug = require('debug')('hs:app');
 
 dotenv.config();
-
-if (!process.env.GITHUB_TOKEN) {
-  throw new Error('Supply GitHub token to start server');
+const requiredEnvVars = ['GITHUB_TOKEN', 'GITHUB_CLIENT_ID', 'GITHUB_CLIENT_SECRET'];
+if(!requiredEnvVars.every(v => process.env[v])) {
+  throw new Error(`Supply env vars: ${requiredEnvVars}`);
 }
+const optionalEnvVarDefaults = {
+  'GITHUB_OAUTH_CALLBACK': 'http://127.0.0.1:3000/auth/github/callback'
+};
+Object.entries(optionalEnvVarDefaults).forEach(([envVar, value]) => {
+  if (!process.env[envVar]) debug(`${envVar} defaulting to: ${value}`);
+});
 
 const app = express();
 
